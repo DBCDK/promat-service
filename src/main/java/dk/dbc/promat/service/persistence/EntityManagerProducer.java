2da45f6bb471a616d0bb1f1b67388272c7c918a0
Jan Bauer Nielsen <jbn@dbc.dk>
2020-10-07 10:30:29 +0200

    Add EntityManager CDI producer
    Auditors: atm, hwha, jbr, mib

---
+++ src/main/java/dk/dbc/promat/service/persistence/EntityManagerProducer.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.promat.service.persistence;
+
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * To obtain an {@link EntityManager} for the Promat database simply say
+ * <pre>
+ * {@literal @}Inject {@literal @}PromatEntityManager EntityManager em
+ * </pre>
+ */
+public class EntityManagerProducer {
+    @Produces @PromatEntityManager
+    @PersistenceContext(unitName="promatPU")
+    private EntityManager promatEntityManager;
+}
