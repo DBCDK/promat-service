571b81aa268b256d7e87c49bb225a8130f393310
Jan Bauer Nielsen <jbn@dbc.dk>
2020-11-04 10:52:12 +0100

    Move common properties to user base class
    Auditors: atm, hwha, jbr, mib

--- src/main/java/dk/dbc/promat/service/persistence/PromatUser.java
+++ src/main/java/dk/dbc/promat/service/persistence/PromatUser.java
@@ -27,7 +27,12 @@ public abstract class PromatUser {
     // update/insert is managed by discriminator mechanics
     @Column(nullable = false, insertable = false, updatable = false)
     @Convert(converter = RoleConverter.class)
-    public Role role;
+    protected Role role;
+
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String phone;
 
     public Integer getId() {
         return id;
@@ -35,5 +40,37 @@ public abstract class PromatUser {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
--- src/main/java/dk/dbc/promat/service/persistence/Reviewer.java
+++ src/main/java/dk/dbc/promat/service/persistence/Reviewer.java
@@ -26,10 +26,6 @@ public class Reviewer extends PromatUser {
     public static final String GET_ALL_REVIEWERS_QUERY =
             "SELECT reviewer FROM Reviewer reviewer ORDER BY reviewer.id ASC";
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
 
     @Embedded
     private Address address;
@@ -48,38 +44,6 @@ public class Reviewer extends PromatUser {
             inverseJoinColumns = @JoinColumn(name = "subject_id")
     )
     private Collection<Subject> subjects;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 
     public Address getAddress() {
         return address;
