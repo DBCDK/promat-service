d3b4a4303e3dd0a53a8e292319f7b2e15ed5674f
Jesper Bruun <jbr@dbc.dk>
2020-11-09 13:39:06 +0100

    Move subject used in reviewer test to subjects sql dump. Auditors: atm, jbn, hwha, mib

--- src/test/resources/dk/dbc/promat/service/db/subjects/subjectsdump.sql
+++ src/test/resources/dk/dbc/promat/service/db/subjects/subjectsdump.sql
@@ -2,3 +2,4 @@ insert into subject(id, name, parentid) VALUES (1, 'Voksen', null);
 insert into subject(id, name, parentid) VALUES (2, 'Roman', 1);
 insert into subject(id, name, parentid) VALUES (3, 'Eventyr, fantasy', 2);
 insert into subject(id, name, parentid) VALUES (4, 'Digte', 1);
+insert into subject(id, name, parentid) VALUES (5, 'Multimedie', null);
