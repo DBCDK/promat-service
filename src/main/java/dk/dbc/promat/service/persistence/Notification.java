abe96c7efed8c61e7393c1f83d63435b137c8c40
Henrik Witt-Hansen <hwha@dbc.dk>
2020-11-26 11:52:58 +0100

    Add missing sourcefile headers
    Auditors: jbn, atm, jbr

--- src/main/java/dk/dbc/promat/service/persistence/Notification.java
+++ src/main/java/dk/dbc/promat/service/persistence/Notification.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
 package dk.dbc.promat.service.persistence;
 
 import dk.dbc.promat.service.templating.NotificationFactory;
