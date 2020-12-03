08e342718af9bf4244c87f0d6fb65ce757c88489
Henrik Witt-Hansen <hwha@dbc.dk>
2020-12-03 17:31:07 +0100

    Add endpoint for updating a task
    With an initial (simple) test.
    
    Auditors: jbn, atm, jbr

---
+++ src/main/java/dk/dbc/promat/service/api/Tasks.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.promat.service.api;
+
+import dk.dbc.promat.service.dto.ServiceErrorDto;
+import dk.dbc.promat.service.dto.TaskDto;
+import dk.dbc.promat.service.persistence.PromatEntityManager;
+import dk.dbc.promat.service.persistence.Repository;
+import dk.dbc.promat.service.persistence.PromatTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("")
+public class Tasks {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Cases.class);
+
+    @Inject
+    @PromatEntityManager
+    EntityManager entityManager;
+
+    @EJB Repository repository;
+
+    @POST
+    @Path("tasks/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateTask(@PathParam("id") final Integer id, TaskDto dto) {
+        LOGGER.info("tasks/{} (POST) body: {}", id, dto);
+
+        try {
+
+            // Fetch the existing task with the given id
+            PromatTask existing = entityManager.find(PromatTask.class, id);
+            if( existing == null ) {
+                LOGGER.info("No such task {}", id);
+                return ServiceErrorDto.NotFound("No such task", String.format("Task with id {} does not exist", id));
+            }
+
+            // Update fields
+            if(dto.getData() != null) {
+                existing.setData(dto.getData()); // It is allowed to update with an empty value
+            }
+
+            return Response.ok(existing).build();
+        }
+        catch(Exception exception) {
+            LOGGER.error("Caught exception: {}", exception.getMessage());
+            return ServiceErrorDto.Failed(exception.getMessage());
+        }
+    }
+}
---
+++ src/test/java/dk/dbc/promat/service/TasksIT.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.promat.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dk.dbc.promat.service.dto.CaseRequestDto;
+import dk.dbc.promat.service.dto.CaseSummaryList;
+import dk.dbc.promat.service.dto.TaskDto;
+import dk.dbc.promat.service.persistence.CaseStatus;
+import dk.dbc.promat.service.persistence.MaterialType;
+import dk.dbc.promat.service.persistence.PromatCase;
+import dk.dbc.promat.service.persistence.PromatTask;
+import dk.dbc.promat.service.persistence.Subject;
+import dk.dbc.promat.service.persistence.TaskFieldType;
+import dk.dbc.promat.service.persistence.TaskType;
+import org.hamcrest.core.IsNull;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+
+public class TasksIT extends ContainerTest {
+
+    @Test
+    public void testEditTask() throws JsonProcessingException {
+
+        // Update of unknown task - should return 404 NOT FOUND
+        TaskDto dto = new TaskDto();
+        Response response = postResponse("v1/api/tasks/9876", dto);
+        assertThat("status code", response.getStatus(), is(404));
+
+        // Create a new case
+        CaseRequestDto caseDto = new CaseRequestDto()
+                .withTitle("Title for 11001111")
+                .withDetails("Details for 11001111")
+                .withPrimaryFaust("11001111")
+                .withRelatedFausts(Arrays.asList("11002222"))
+                .withMaterialType(MaterialType.BOOK)
+                .withTasks(Arrays.asList(
+                        new TaskDto()
+                                .withTaskType(TaskType.GROUP_1_LESS_THAN_100_PAGES)
+                                .withTaskFieldType(TaskFieldType.BRIEF),
+                        new TaskDto()
+                                .withTaskType(TaskType.GROUP_1_LESS_THAN_100_PAGES)
+                                .withTaskFieldType(TaskFieldType.BRIEF)
+                                .withTargetFausts(Arrays.asList(new String[] {"11002222", "8004444"}))
+                ));
+        response = postResponse("v1/api/cases", caseDto);
+        assertThat("status code", response.getStatus(), is(201));
+        PromatCase createdCase = mapper.readValue(response.readEntity(String.class), PromatCase.class);
+
+        // Find task ids
+        PromatTask taskNoTargetFaust = createdCase.getTasks().stream().filter(task -> task.getTargetFausts() == null).findFirst().get();
+        PromatTask taskWithTargetFaust = createdCase.getTasks().stream().filter(task -> task.getTargetFausts() != null && task.getTargetFausts().size() != 0).findFirst().get();
+        assertThat("has task without targetFaust", taskNoTargetFaust, is(notNullValue()));
+        assertThat("has task with targetFaust", taskNoTargetFaust, is(notNullValue()));
+
+        // Update first task - should return 200 OK
+        dto = new TaskDto().withData("Here is data for task without targetFaust");
+        response = postResponse("v1/api/tasks/" + taskNoTargetFaust.getId(), dto);
+        assertThat("status code", response.getStatus(), is(200));
+        PromatTask updated = mapper.readValue(response.readEntity(String.class), PromatTask.class);
+        assertThat("data value is correct", updated.getData().equals("Here is data for task without targetFaust"), is(true));
+
+        // Update second task - should return 200 OK
+        dto = new TaskDto().withData("Here is data for task with targetFaust");
+        response = postResponse("v1/api/tasks/" + taskWithTargetFaust.getId(), dto);
+        assertThat("status code", response.getStatus(), is(200));
+        updated = mapper.readValue(response.readEntity(String.class), PromatTask.class);
+        assertThat("data value is correct", updated.getData().equals("Here is data for task with targetFaust"), is(true));
+    }
+
+}
