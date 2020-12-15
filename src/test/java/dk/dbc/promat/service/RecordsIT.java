30d4d0a286286fd071b93131064ed7782a037008
Henrik Witt-Hansen <hwha@dbc.dk>
2020-12-15 08:39:32 +0100

    Add test for the records endpoint
    Tests will fail at time of commit due to to the incorrect version
    of HttpClient used by the opensearch and workpresentation connectors
    
    Mapping of service request/respons must be made after having updated
    the connectors and the test finalized at that stage
    
    Auditors: jbn, atm, jbr

---
+++ src/test/java/dk/dbc/promat/service/RecordsIT.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.promat.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dk.dbc.promat.service.dto.RecordsListDto;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class RecordsIT extends ContainerTest {
+
+    @Test
+    public void testResolveFaust() throws JsonProcessingException {
+
+        Response response = getResponse("v1/api/records/24699773");
+        assertThat("status code", response.getStatus(), is(200));
+        RecordsListDto resolved = mapper.readValue(response.readEntity(String.class), RecordsListDto.class);
+
+        assertThat("results", resolved.getNumFound(), is(1));
+    }
+
+    @Test
+    public void testResolveIsbn() throws JsonProcessingException {
+
+        Response response = getResponse("v1/api/records/9788764432589");
+        assertThat("status code", response.getStatus(), is(200));
+        RecordsListDto resolved = mapper.readValue(response.readEntity(String.class), RecordsListDto.class);
+
+        assertThat("results", resolved.getNumFound(), is(1));
+    }
+
+    @Test
+    public void testResolveBarcode() throws JsonProcessingException {
+
+        Response response = getResponse("v1/api/records/5053083221386");
+        assertThat("status code", response.getStatus(), is(200));
+        RecordsListDto resolved = mapper.readValue(response.readEntity(String.class), RecordsListDto.class);
+
+        assertThat("results", resolved.getNumFound(), is(1));
+    }
+}
