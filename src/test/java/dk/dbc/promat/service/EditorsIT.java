facf0f70830a2e832a31bf886baa71dc2627086f
Henrik Witt-Hansen <hwha@dbc.dk>
2020-12-22 10:29:31 +0100

    Update editors
    Auditors: jbn, atm, jbr

--- src/test/java/dk/dbc/promat/service/EditorsIT.java
+++ src/test/java/dk/dbc/promat/service/EditorsIT.java
@@ -5,15 +5,28 @@
 
 package dk.dbc.promat.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dk.dbc.promat.service.dto.EditorRequest;
+import dk.dbc.promat.service.dto.ReviewerRequest;
+import dk.dbc.promat.service.persistence.Address;
 import dk.dbc.promat.service.persistence.Editor;
+import dk.dbc.promat.service.persistence.Reviewer;
+import dk.dbc.promat.service.persistence.Subject;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class EditorsIT extends ContainerTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EditorsIT.class);
 
     @Test
     void getEditor() {
@@ -32,5 +45,33 @@ public class EditorsIT extends ContainerTest {
         final Response response = getResponse("v1/api/editors/4242");
 
         assertThat(response.getStatus(), is(404));
+    }
+
+    @Test
+    void updateEditor() throws JsonProcessingException {
+
+        final EditorRequest editorRequest = new EditorRequest()
+                .withActive(false)
+                .withEmail("edito.r@dbc.dk")
+                .withFirstName("Edito")
+                .withLastName("r");
+
+        final Response response = putResponse("v1/api/editors/12", editorRequest);
+        assertThat("response status", response.getStatus(), is(200));
+        final Editor updated = mapper.readValue(response.readEntity(String.class), Editor.class);
+
+        final Editor expected = new Editor();
+        loadUpdatedEditor(expected);
+
+        assertThat("Editor has been updated", updated.equals(expected));
+    }
+
+    private void loadUpdatedEditor(Editor editor) {
+        editor.setId(12);
+        editor.setActive(false);
+        editor.setCulrId("53");
+        editor.setFirstName("Edito");
+        editor.setLastName("r");
+        editor.setEmail("edito.r@dbc.dk");
     }
 }
