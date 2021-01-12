487c88c72d650a12c6ff0506319b1581fd7b13f1
Henrik Witt-Hansen <hwha@dbc.dk>
2021-01-12 22:56:50 +0100

    Apply changes to deliver information needed by the gui
    The gui needs to show, not only the payments, but also which reviewers
    will receive the payment, and for which faustnumbers is the payments
    created.
    
    There is also a need for sorting the payments so that payments for a specific user
    or faustnumber can quickly be identified. Allthough it can be done by the gui,
    I find it better to handle this in the backend to guarantee that the gui never
    modifies payment information, which could lead to confusion or erroneous case modifications
    
    Auditors: jbn, atm, jbr

--- service/src/main/java/dk/dbc/promat/service/api/Payments.java
+++ service/src/main/java/dk/dbc/promat/service/api/Payments.java
@@ -5,10 +5,17 @@
 
 package dk.dbc.promat.service.api;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.dbc.promat.service.Repository;
+import dk.dbc.promat.service.dto.ServiceErrorDto;
+import dk.dbc.promat.service.persistence.CaseView;
 import dk.dbc.promat.service.persistence.PromatCase;
 import dk.dbc.promat.service.persistence.PromatEntityManager;
 import dk.dbc.promat.service.persistence.PromatTask;
+import dk.dbc.promat.service.persistence.Reviewer;
+import dk.dbc.promat.service.persistence.TaskFieldType;
+import dk.dbc.promat.service.rest.JsonMapperProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,15 +25,18 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Stateless
-@Path("payments")
+@Path("")
 public class Payments {
     private static final Logger LOGGER = LoggerFactory.getLogger(Payments.class);
 
@@ -37,18 +47,33 @@ public class Payments {
     @EJB
     Repository repository;
 
-    enum PaymentsFormat {
+    public enum PaymentsFormat {
         CSV,
-        JSON  // Todo: need a better name methinks.. but awaiting ux input before deciding
+        PAYMENT_LIST,
+        PAYMENT_LIST_BY_USER,
+        PAYMENT_LIST_BY_FAUST
     }
 
     private class Payment {
-        // Todo: This class will be refined as usage is clarified
 
         String payCode;
+
         String payType;
+
         int count;
+
         String text;
+
+        @JsonView({CaseView.CaseSummary.class})
+        Reviewer reviewer;
+
+        String primaryFaust;
+
+        String title;
+
+        String weekCode;
+
+        TaskFieldType taskFieldType;
 
         public String getPayCode() {
             return payCode;
@@ -82,6 +107,46 @@ public class Payments {
             this.text = text;
         }
 
+        public Reviewer getReviewer() {
+            return reviewer;
+        }
+
+        public void setReviewer(Reviewer reviewer) {
+            this.reviewer = reviewer;
+        }
+
+        public String getPrimaryFaust() {
+            return primaryFaust;
+        }
+
+        public void setPrimaryFaust(String primaryFaust) {
+            this.primaryFaust = primaryFaust;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getWeekCode() {
+            return weekCode;
+        }
+
+        public void setWeekCode(String weekCode) {
+            this.weekCode = weekCode;
+        }
+
+        public TaskFieldType getTaskFieldType() {
+            return taskFieldType;
+        }
+
+        public void setTaskFieldType(TaskFieldType taskFieldType) {
+            this.taskFieldType = taskFieldType;
+        }
+
         public Payment withPayCode(String payCode) {
             this.payCode = payCode;
             return this;
@@ -101,12 +166,38 @@ public class Payments {
             this.text = text;
             return this;
         }
+
+        public Payment withReviewer(Reviewer reviewer) {
+            this.reviewer = reviewer;
+            return this;
+        }
+
+        public Payment withPrimaryFaust(String primaryFaust) {
+            this.primaryFaust = primaryFaust;
+            return this;
+        }
+
+        public Payment withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Payment withWeekCode(String weekCode) {
+            this.weekCode = weekCode;
+            return this;
+        }
+
+        public Payment withTaskFieldType(TaskFieldType taskFieldType) {
+            this.taskFieldType = taskFieldType;
+            return this;
+        }
     }
 
     private class PaymentList {
 
         private int numFound;
-        private List<Payment> payments = new ArrayList<Payment>();
+
+        private List<Payment> payments = new ArrayList<>();
 
         public int getNumFound() {
             return numFound;
@@ -135,42 +226,177 @@ public class Payments {
         }
     }
 
+    private class GroupedPaymentList {
+
+        private int numFound;
+
+        private List<PaymentList> groups = new ArrayList<>();
+
+        public int getNumFound() {
+            return numFound;
+        }
+
+        public void setNumFound(int numFound) {
+            this.numFound = numFound;
+        }
+
+        public List<PaymentList> getGroups() {
+            return groups;
+        }
+
+        public void setGroups(List<PaymentList> groups) {
+            this.groups = groups;
+        }
+
+        public GroupedPaymentList withNumFound(int numFound) {
+            this.numFound = numFound;
+            return this;
+        }
+
+        public GroupedPaymentList withGroups(List<PaymentList> groups) {
+            this.groups = groups;
+            return this;
+        }
+    }
+
     @GET
-    @Path("preview")
-    public Response preview(@QueryParam("format") PaymentsFormat format) {
-        format = format != null ? format : PaymentsFormat.JSON;
-        LOGGER.info("payments/preview/?format={} (GET)", format);
+    @Path("payments/preview")
+    public Response preview(@QueryParam("format") final PaymentsFormat selectedformat) {
+        LOGGER.info("ENTRY");
+        PaymentsFormat format = selectedformat != null ? selectedformat : PaymentsFormat.PAYMENT_LIST;
+        LOGGER.info("payments/preview/?format={} (GET)", selectedformat);
+
+        PaymentList paymentList;
+        try {
+            paymentList = getPendingPayments(false);
+            LOGGER.info("Has {} pending payments", paymentList.getNumFound());
+        }
+        catch(Exception e) {
+            LOGGER.error("Unexpected exception while retrieving payments: {}", e.getMessage());
+            return ServiceErrorDto.Failed("Unexpected exception while retrieving payments");
+        }
+
+        try {
+            if(format == PaymentsFormat.CSV) {
+                return Response.status(200)
+                        .header("Pragma", "no-cache")
+                        .header("Content-Type", "application/csv")
+                        .header("Content-Disposition", "attachment; filename=" + getPaymentsCsvFilename("promat_payments_PREVIEW"))
+                        .entity(convertPaymentListToCsv(paymentList))
+                        .build();
+            } else if(format == PaymentsFormat.PAYMENT_LIST) {
+                return Response.status(200)
+                        .header("Pragma", "no-cache")
+                        .header("Content-Type", "application/json")
+                        .entity(paymentList)
+                        .build();
+            } else if(format == PaymentsFormat.PAYMENT_LIST_BY_USER) {
+                ObjectMapper mapper = new JsonMapperProvider().getObjectMapper();
+                return Response.status(200)
+                        .header("Pragma", "no-cache")
+                        .header("Content-Type", "application/json")
+                        .entity(mapper.writerWithView(CaseView.CaseSummary.class)
+                                .writeValueAsString(groupPaymentsByUser(paymentList)))
+                        .build();
+            } else if(format == PaymentsFormat.PAYMENT_LIST_BY_FAUST) {
+                ObjectMapper mapper = new JsonMapperProvider().getObjectMapper();
+                return Response.status(200)
+                        .header("Pragma", "no-cache")
+                        .header("Content-Type", "application/json")
+                        .entity(mapper.writerWithView(CaseView.CaseSummary.class)
+                                .writeValueAsString(groupPaymentsByFaust(paymentList)))
+                        .build();
+            } else {
+                // Aka. "Developer note": "You forgot something!"
+                LOGGER.info("Payment format {} is not handled by preview()", format);
+                return ServiceErrorDto.InvalidRequest("Unknown format", "The given format is not handled by this endpoint");
+            }
+        }
+        catch(Exception e) {
+            LOGGER.error("Unexpected exception while building payments list: {}", e.getMessage());
+            return ServiceErrorDto.Failed("Unexpected exception while building payments list");
+        }
+    }
+
+    private GroupedPaymentList groupPaymentsByUser(PaymentList paymentList) {
+        Map<Integer, List<Payment>> grouped = paymentList
+                .getPayments()
+                .stream()
+                .collect(Collectors.groupingBy(payment -> payment.getReviewer().getId()));
+
+        List<PaymentList> paymentLists = grouped
+                .values()
+                .stream()
+                .map(group -> new PaymentList()
+                        .withNumFound(group.size())
+                        .withPayments(group))
+                .collect(Collectors.toList());
+
+        return new GroupedPaymentList()
+                .withNumFound(paymentLists.size())
+                .withGroups(paymentLists);
+    }
+
+    private GroupedPaymentList groupPaymentsByFaust(PaymentList paymentList) {
+        Map<String, List<Payment>> grouped = paymentList
+                .getPayments()
+                .stream()
+                .collect(Collectors.groupingBy(Payment::getPrimaryFaust));
+
+        List<PaymentList> paymentLists = grouped
+                .values()
+                .stream()
+                .map(group -> new PaymentList()
+                        .withNumFound(group.size())
+                        .withPayments(group))
+                .collect(Collectors.toList());
+
+        return new GroupedPaymentList()
+                .withNumFound(paymentLists.size())
+                .withGroups(paymentLists);
+    }
+
+    private String getPaymentsCsvFilename(String prefix) {
+        return prefix + String.format("_%s", LocalDateTime.now()
+                .format(DateTimeFormatter
+                        .ofPattern("yyyyMMdd_HHmmssSSS"))) + ".csv";
+    }
+
+    private String convertPaymentListToCsv(PaymentList paymentList) {
+        return "Lønnr;Lønart;Antal;;;Tekst\n" +
+                paymentList.getPayments()
+                        .stream()
+                        .map(payment -> String.format("%s;%s;%d;%s\n",
+                                payment.getPayCode(),
+                                payment.getPayType(),
+                                payment.getCount(),
+                                payment.getText()))
+                        .collect(Collectors.joining());
+    }
+
+    private PaymentList getPendingPayments(boolean execute) {
 
         // Lock all relevant tables
-        // Todo: will move to central function, and applied only if needed
-        repository.getExclusiveAccessToTable(PromatCase.TABLE_NAME);
-        repository.getExclusiveAccessToTable(PromatTask.TABLE_NAME);
-
-        if(format == PaymentsFormat.CSV) {
-            // Todo: Example response, will be dynamiccaly built and formatted before this point
-            String csv = "Lønnr;Lønart;Antal;;;Tekst\n";
-
-            return Response.status(200)
-                    .header("Content-Type", "application/csv")
-                    .header("Content-Disposition", "attachment; filename=payments_preview.csv")  // Todo: Append calendar period to filename
-                    .header("Pragma", "no-cache")
-                    .entity(csv)
-                    .build();
-        } else {
-            // Todo: Example response, will be dynamically built and formatted before this point
-            final PaymentList paymentList = new PaymentList()
-                    .withNumFound(1)
-                    .withPayments(Arrays.asList(new Payment()
-                            .withPayCode("1234")
-                            .withPayType("5678")
-                            .withCount(3)
-                            .withText("example payment")));
-
-            return Response.status(200)
-                    .header("Content-Type", "application/json")
-                    .header("Pragma", "no-cache")
-                    .entity(paymentList)
-                    .build();
+        if(execute) {
+            LOGGER.info("Locking case and task tables for payment execution");
+            repository.getExclusiveAccessToTable(PromatCase.TABLE_NAME);
+            repository.getExclusiveAccessToTable(PromatTask.TABLE_NAME);
         }
+
+        // Todo: Example data, will be dynamiccaly built
+        final PaymentList paymentList = new PaymentList()
+                .withNumFound(1)
+                .withPayments(Arrays.asList(new Payment()
+                        .withPayCode("1234")
+                        .withPayType("5678")
+                        .withCount(3)
+                        .withText("example payment")
+                        .withReviewer(entityManager.find(Reviewer.class, 1))
+                        .withPrimaryFaust("123")
+                        .withTitle("title")
+                        .withWeekCode("BKM202101")
+                        .withTaskFieldType(TaskFieldType.BRIEF)));
+
+        return paymentList;
     }
 }
