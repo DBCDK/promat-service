1fbeb760f6e51760910905760412e65409c3ca82
Jesper Bruun <jbr@dbc.dk>
2021-01-15 15:47:57 +0100

    pro2-149-send-messages: Move migration to 22. Auditors: atm, jbn, hwha, mib

---
+++ service/src/main/resources/dk/dbc/promat/service/db/migration/V22__add__messages_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE promatmessage
+(
+    id  SERIAL PRIMARY KEY NOT NULL,
+    reviewer_id INTEGER NOT NULL,
+    editor_id INTEGER NOT NULL ,
+    promatcase_id INTEGER NOT NULL,
+    messageText text,
+    created date DEFAULT NOW(),
+    isread BOOLEAN DEFAULT FALSE,
+    direction TEXT NOT NULL
+);
+
+-- Set starting sequence for new messages at 500000
+ALTER SEQUENCE promatmessage_id_seq RESTART WITH 500000;
+
+CREATE INDEX ON promatmessage (promatcase_id);
+
+ALTER TABLE promatmessage ADD FOREIGN KEY (reviewer_id) REFERENCES promatuser(id);
+ALTER TABLE promatmessage ADD FOREIGN KEY (editor_id) REFERENCES promatuser(id);
+ALTER TABLE promatmessage ADD FOREIGN KEY (promatcase_id) REFERENCES promatcase(id) ON DELETE CASCADE;
