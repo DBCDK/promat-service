e38a31c0ff1d300c3e619875b8e5efb3d87097b9
Henrik Witt-Hansen <hwha@dbc.dk>
2021-02-11 12:05:57 +0100

    Add formatter for providing the full name for a promatuser
    Auditors: jbn, atm, jbr

--- service/src/main/java/dk/dbc/promat/service/templating/Formatting.java
+++ service/src/main/java/dk/dbc/promat/service/templating/Formatting.java
@@ -5,12 +5,30 @@
 
 package dk.dbc.promat.service.templating;
 
+import dk.dbc.promat.service.persistence.PromatUser;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Formatting {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M yyyy");
     public static String format(LocalDate localDate) {
         return localDate.format(formatter);
+    }
+
+    public static <T extends PromatUser> String format(T user) {
+        var names = new ArrayList<String>();
+
+        if( user.getFirstName() != null ) {
+            names.addAll(Arrays.asList(user.getFirstName().split(" ")));
+        }
+        if( user.getLastName() != null ) {
+            names.addAll(Arrays.asList(user.getLastName().split(" ")));
+        }
+
+        return names.size() > 0 ? names.stream().collect(Collectors.joining(" ")) : "";
     }
 }
