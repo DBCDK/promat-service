5acbc0026b5a6de80c8b990b117a9981b4aaa040
Jesper Bruun <jbr@dbc.dk>
2021-02-22 09:20:24 +0100

    Fix notificatiosender bug When an attempt at sending a notification as mail would fail, and the notification could be sent at next attempt, the status of the notification was never changed: So the mail would be mailed again and again every 5 minutes.
    Fix: Not sure why it works: Skip the merge, and pop notification from
    queue that are either in 'pending' or 'error'. (That is: Do not change back from
    'error' to 'pending', before attempting a send-mail again.)
    
    Auditors: atm, jbn, hwha, mib

--- service/src/test/resources/dk/dbc/promat/service/db/notification.sql
+++ service/src/test/resources/dk/dbc/promat/service/db/notification.sql
@@ -1,3 +1,4 @@
 INSERT INTO notification(bodytext, subject, toaddress, status, created) values ('<h1>hej1</h1>', 'test1', 'test1@test.dk', 0, now());
 INSERT INTO notification(bodytext, subject, toaddress, status, created) values ('<h1>hej2</h1>', 'test2', 'test2@test.dk', 1, now());
 INSERT INTO notification(bodytext, subject, toaddress, status, created) values ('<h1>hej3</h1>', 'test3', 'test3@test.dk', 0, now());
+INSERT INTO notification(bodytext, subject, toaddress, status, created) values ('<h1>hej4</h1>', 'test4', 'test4@test.dk', 2, now());
