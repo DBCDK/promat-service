9853f564c2cfce1d2c67cf262bfad7bad10ddf60
Jesper Bruun <jbr@dbc.dk>
2021-03-04 11:10:07 +0100

    Fix bug: getRelated would return main faust, if related on task was [] And also make methods static (avoid injection).
    Auditors: atm, jbn, hwha, mib

--- model/src/main/java/dk/dbc/promat/service/util/PromatTaskUtils.java
+++ model/src/main/java/dk/dbc/promat/service/util/PromatTaskUtils.java
@@ -3,32 +3,29 @@ package dk.dbc.promat.service.util;
 import dk.dbc.promat.service.persistence.PromatCase;
 import dk.dbc.promat.service.persistence.PromatTask;
 import dk.dbc.promat.service.persistence.TaskFieldType;
-import java.util.List;
 import java.util.Optional;
-import javax.enterprise.context.ApplicationScoped;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ApplicationScoped
 public class PromatTaskUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(PromatTaskUtils.class);
 
-    public Optional<PromatTask> getTaskForMainFaust(PromatCase promatCase, TaskFieldType taskFieldType) {
+    public static Optional<PromatTask> getTaskForMainFaust(PromatCase promatCase, TaskFieldType taskFieldType) {
         return promatCase.getTasks()
                 .stream()
-                .filter(promatTask ->
-                        promatTask.getTaskFieldType() == taskFieldType &&
-                                (promatTask.getTargetFausts() == null
-                                        || promatTask.getTargetFausts().isEmpty())).findFirst();
+                .filter(promatTask -> promatTask.getTaskFieldType() == taskFieldType)
+                .filter(promatTask -> promatTask.getTargetFausts() == null || promatTask.getTargetFausts().isEmpty())
+                .findFirst();
 
 
     }
 
-    public Optional<PromatTask> getTaskForRelatedFaust(PromatCase promatCase, TaskFieldType taskFieldType) {
+    public static Optional<PromatTask> getTaskForRelatedFaust(PromatCase promatCase, TaskFieldType taskFieldType) {
         return promatCase.getTasks()
                 .stream()
-                .filter(promatTask -> promatTask.getTaskFieldType() == taskFieldType &&
-                        (promatTask.getTargetFausts() != null )).findFirst();
+                .filter(promatTask -> promatTask.getTaskFieldType() == taskFieldType)
+                .filter(promatTask -> promatTask.getTargetFausts() != null && !promatTask.getTargetFausts().isEmpty())
+                .findFirst();
     }
 
 }
