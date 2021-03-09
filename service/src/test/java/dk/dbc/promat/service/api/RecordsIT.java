54d37a1b84489485aed7dfdac24fefd4cca74e46
Henrik Witt-Hansen <hwha@dbc.dk>
2021-03-09 18:47:41 +0100

    Fix inconsistent return from records endpoint
    by always returning a list in the records property, even if there is
    no results
    
    Auditors: jbn, atm, jbr

--- service/src/test/java/dk/dbc/promat/service/api/RecordsIT.java
+++ service/src/test/java/dk/dbc/promat/service/api/RecordsIT.java
@@ -15,6 +15,8 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.core.Response;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 
 public class RecordsIT extends ContainerTest {
@@ -63,5 +65,28 @@ public class RecordsIT extends ContainerTest {
         RecordsListDto resolved = mapper.readValue(response.readEntity(String.class), RecordsListDto.class);
 
         assertThat("results", resolved.getNumFound(), is(2));
+    }
+
+    @Test
+    public void testConsistentReturnForNonexistingFausts() throws JsonProcessingException {
+
+        // There is situations where opensearch has results, but work-presentation
+        // has none, and so the final result is 'no records'. But the records property
+        // is then set to either a null OR an empty array.. This is very confusing for
+        // the frontend. Check that this situation has been fixed
+
+        Response response = getResponse("v1/api/records/123");
+        assertThat("status code", response.getStatus(), is(200));
+        RecordsListDto resolved_123 = mapper.readValue(response.readEntity(String.class), RecordsListDto.class);
+        assertThat("results", resolved_123.getNumFound(), is(0));
+        assertThat("records", resolved_123.getRecords(), is(not(nullValue())));
+        assertThat("records length", resolved_123.getRecords().size(), is(0));
+
+        response = getResponse("v1/api/records/226777809");
+        assertThat("status code", response.getStatus(), is(200));
+        RecordsListDto resolved_226777809 = mapper.readValue(response.readEntity(String.class), RecordsListDto.class);
+        assertThat("results", resolved_226777809.getNumFound(), is(0));
+        assertThat("records", resolved_226777809.getRecords(), is(not(nullValue())));
+        assertThat("records length", resolved_226777809.getRecords().size(), is(0));
     }
 }
