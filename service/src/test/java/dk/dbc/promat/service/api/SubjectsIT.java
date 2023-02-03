f967de383ed4743207c4b41da1a02f99a1f02ae0
Henrik Witt-Hansen <hwha@dbc.dk>
2023-02-03 16:08:59 +0100

    Fix tests now that we seed the database with all initial subjects

--- service/src/test/java/dk/dbc/promat/service/api/SubjectsIT.java
+++ service/src/test/java/dk/dbc/promat/service/api/SubjectsIT.java
@@ -1,20 +1,14 @@
-/*
- * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
- * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
- */
-
 package dk.dbc.promat.service.api;
 
 import dk.dbc.commons.jsonb.JSONBContext;
 import dk.dbc.commons.jsonb.JSONBException;
 import dk.dbc.promat.service.ContainerTest;
 import dk.dbc.promat.service.dto.SubjectList;
-import dk.dbc.promat.service.persistence.Subject;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -28,19 +22,18 @@ public class SubjectsIT extends ContainerTest {
 
     @Test
     public void test_referential_hierarchy() throws JSONBException {
-        SubjectList expected = new SubjectList().withSubjects(
-                List.of(
-                        new Subject().withId(1).withName("Voksen"),
-                        new Subject().withId(2).withName("Roman").withParentId(1),
-                        new Subject().withId(3).withName("Eventyr, fantasy").withParentId(2),
-                        new Subject().withId(4).withName("Digte").withParentId(1),
-                        new Subject().withId(5).withName("Multimedie"),
-                        new Subject().withId(6).withName("(Et Mulitmedie underemne)").withParentId(5)
-                ));
-
         SubjectList actual = jsonbContext
                 .unmarshall(get(String.format("%s/%s", promatServiceBaseUrl, "v1/api/subjects")), SubjectList.class);
-        assertThat("Service returns all persisted subjects", actual.toString(), is(expected.toString()));
+
+        // Must contain 5 mock subjects with id 1-6 for testing and 475 production values from id 57 and up
+        assertThat("Service returns all persisted subjects", actual.getSubjects().size(), is(480));
+
+        // Check a few production values
+        assertThat("contains subject 'Voksen'", (int) actual.getSubjects().stream()
+                .filter(s -> s.getId() == 57 && Objects.equals(s.getName(), "Voksen")).count(), is(1));
+        assertThat("contains subject '26 Kirkens institutioner'", (int) actual.getSubjects().stream()
+                .filter(s -> s.getId() == 357 && Objects.equals(s.getName(), "26 Kirkens institutioner")).count(), is(1));
+        assertThat("contains subject 'Fagfilm/børn'", (int) actual.getSubjects().stream()
+                .filter(s -> s.getId() == 1691 && Objects.equals(s.getName(), "Fagfilm/børn")).count(), is(1));
     }
 }
-
