8db6a111527c34868673654bcfffae8285a79b11
jbr <jbr@dbc.dk>
2025-11-14 15:25:35 +0100

    Nye lønkoder

--- model/src/main/java/dk/dbc/promat/service/persistence/PayCategory.java
+++ model/src/main/java/dk/dbc/promat/service/persistence/PayCategory.java
@@ -3,37 +3,37 @@ package dk.dbc.promat.service.persistence;
 public enum PayCategory {
     NONE("0000"),
 
-    BRIEF("1960"),
+    BRIEF("3216"),
 
-    METAKOMPAS("1987"),
+    METAKOMPAS("3229"),
 
-    BUGGI("1988"),
+    BUGGI("3230"),
 
-    BKM("1962"),
+    BKM("3218"),
 
-    GROUP_1_LESS_THAN_100_PAGES("1956"),
+    GROUP_1_LESS_THAN_100_PAGES("3212"),
 
-    GROUP_2_100_UPTO_199_PAGES("1957"),
+    GROUP_2_100_UPTO_199_PAGES("3213"),
 
-    GROUP_3_200_UPTO_499_PAGES("1958"),
+    GROUP_3_200_UPTO_499_PAGES("3214"),
 
-    GROUP_4_500_OR_MORE_PAGES("1959"),
+    GROUP_4_500_OR_MORE_PAGES("3215"),
 
-    MOVIES_GR_1("1980"),
+    MOVIES_GR_1("3222"),
 
-    MOVIES_GR_2("1981"),
+    MOVIES_GR_2("3223"),
 
-    MOVIES_GR_3("1982"),
+    MOVIES_GR_3("3224"),
 
-    MULTIMEDIA_FEE("1954"),
+    MULTIMEDIA_FEE("3210"),
 
-    MULTIMEDIA_FEE_GR2("1985"),
+    MULTIMEDIA_FEE_GR2("3227"),
 
-    MOVIE_NON_FICTION_GR1("1979"),
+    MOVIE_NON_FICTION_GR1("3221"),
 
-    MOVIE_NON_FICTION_GR2("1983"),
+    MOVIE_NON_FICTION_GR2("3225"),
 
-    MOVIE_NON_FICTION_GR3("1984"),
+    MOVIE_NON_FICTION_GR3("3226"),
 
     // To be ignored in payments, task with this category does not add extra payments
     EXPRESS("0000");
